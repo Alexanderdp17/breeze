0e05351854507342ebd856d19c21a98f1cceda9a
Alexanderdp17 <0200721054@udh.edu.pe>
2022-09-28 19:36:25 -0500

    first commit

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,19 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            Registro Cursos
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white border-b border-gray-200">
+                    <form>
+                        <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-red-500" type="text" placeholder="ingrese nombre">
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
